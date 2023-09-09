d3545d42ad8044158578a7f4d7b491f0c5b188f1
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-09-09 17:49:42 -0300

    remove id annotation

--- src/main/java/com/academia/academiaalunos/mapper/AlunoMapper.java
+++ src/main/java/com/academia/academiaalunos/mapper/AlunoMapper.java
@@ -2,7 +2,6 @@ package com.academia.academiaalunos.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-import org.mapstruct.Mapping;
 
 import com.academia.academiaalunos.domain.Aluno;
 import com.academia.academiaalunos.requests.AlunoPostRequestBody;
@@ -12,7 +11,6 @@ import com.academia.academiaalunos.requests.AlunoPutRequestBody;
 public abstract class AlunoMapper {
    public static final AlunoMapper INSTANCE  = Mappers.getMapper(AlunoMapper.class);
 
-   @Mapping(target = "id", ignore = true)
    public abstract Aluno toAluno(AlunoPostRequestBody alunoPostRequestBody);
 
    public abstract Aluno toAluno(AlunoPutRequestBody alunoPutRequestBody);
--- src/main/java/com/academia/academiaalunos/mapper/AvaliacaoMapper.java
+++ src/main/java/com/academia/academiaalunos/mapper/AvaliacaoMapper.java
@@ -1,7 +1,6 @@
 package com.academia.academiaalunos.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import com.academia.academiaalunos.domain.Avaliacao;
@@ -12,7 +11,6 @@ import com.academia.academiaalunos.requests.AvaliacaoPutRequestBody;
 public abstract class AvaliacaoMapper {
    public static final AvaliacaoMapper INSTANCE  = Mappers.getMapper(AvaliacaoMapper.class);
 
-   @Mapping(target = "id", ignore = true)
    public abstract Avaliacao toAvaliacao(AvaliacaoPostRequestBody avaliacaoPostRequestBody);
 
    public abstract Avaliacao toAvaliacao(AvaliacaoPutRequestBody avaliacaoPutRequestBody);
