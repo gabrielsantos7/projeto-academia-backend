93e47d06a59927546d645df396b2cdb327cf5b37
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-09-30 10:31:56 -0300

    add crossorigin

--- src/main/java/com/academia/academiaalunos/controller/AlunoController.java
+++ src/main/java/com/academia/academiaalunos/controller/AlunoController.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import java.util.List;
 @RestController
 @RequestMapping("alunos") // http://localhost:8080/alunos
 @Log4j2
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class AlunoController {
     private final DateUtil dateUtil;
--- src/main/java/com/academia/academiaalunos/controller/AvaliacaoController.java
+++ src/main/java/com/academia/academiaalunos/controller/AvaliacaoController.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.*;
 
 import com.academia.academiaalunos.domain.Avaliacao;
@@ -20,6 +21,7 @@ import java.util.List;
 @RestController
 @RequestMapping("avaliacoes") // http://localhost:8080/avaliacoes
 @Log4j2
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class AvaliacaoController {
     private final DateUtil dateUtil;
