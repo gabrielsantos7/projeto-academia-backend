44946d69100850c6c8abbbc56cb80c2b866d77f9
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-10-24 18:20:25 -0300

    remove unusual importations

--- src/main/java/com/academia/academiaalunos/requests/AlunoPostRequestBody.java
+++ src/main/java/com/academia/academiaalunos/requests/AlunoPostRequestBody.java
@@ -7,7 +7,6 @@ import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,7 +18,6 @@ public class AlunoPostRequestBody {
     private String nome;
 
     @NotBlank
-    @Pattern(regexp = "(\\(\\d{2}\\))?\\s?\\d{4,5}-\\d{4}", message = "Formato de telefone inválido")
     private String telefone;
 
     @NotNull
